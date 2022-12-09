2d8e711839ee0c3fc0bf70916f411ae3dec2d076
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2022-12-09 16:08:03 +0100

    Update User.model.js

--- server/models/User.model.js
+++ server/models/User.model.js
@@ -1,14 +1,21 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model } = require("mongoose")
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new Schema({
   email: {
-    type: String, unique: true, required: true
+    type: String,
+    unique: true,
+    required: [true, 'El email es obligatorio']
   },
   password: {
-    type: String, required: true
+    type: String,
+    required: [true, 'La contraseña es obligatoria'],
+    minlength: [5, 'La contraseña debe tener mínimo 5 caracteres']
   },
   username: {
-    type: String, required: true
+    type: String,
+    required: [true, 'El nombre de usuario es obligatorio']
   }
 },
   {
@@ -16,6 +23,36 @@ const userSchema = new Schema({
   }
 )
 
-const User = model("User", userSchema)
+userSchema.pre('save', function (next) {
 
-module.exports = User
+  const saltRounds = 10
+  const salt = bcrypt.genSaltSync(saltRounds)
+  const hashedPassword = bcrypt.hashSync(this.password, salt)
+  this.password = hashedPassword
+
+  next()
+})
+
+
+
+userSchema.methods.validatePassword = function (candidatePassword) {
+  return bcrypt.compareSync(candidatePassword, this.password)
+}
+
+
+
+userSchema.methods.signToken = function () {
+  const { _id, username, email } = this
+  const payload = { _id, username, email }
+
+  const authToken = jwt.sign(
+    payload,
+    process.env.TOKEN_SECRET,
+    { algorithm: 'HS256', expiresIn: "6h" }
+  )
+
+  return authToken
+}
+
+
+module.exports = model("User", userSchema)
