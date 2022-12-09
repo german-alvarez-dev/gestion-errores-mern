58671ae9f18ebc6a5e7ec52ca21b005e445cad0b
Germán Álvarez <46670724+german-alvarez-dev@users.noreply.github.com>
2022-12-09 16:05:44 +0100

    Update auth.routes.js

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,6 @@
 const router = require("express").Router()
 
-const bcrypt = require('bcryptjs')
 const User = require("../models/User.model")
-const saltRounds = 10
-
-const jwt = require('jsonwebtoken')
 
 const { isAuthenticated } = require('./../middleware/jwt.middleware')
 
@@ -14,84 +10,41 @@ router.post('/signup', (req, res, next) => {
 
   const { email, password, username } = req.body
 
-  if (password.length < 2) {
-    res.status(400).json({ message: 'Password must have at least 3 characters' })
-    return
-  }
-
   User
-    .findOne({ email })
-    .then((foundUser) => {
-
-      if (foundUser) {
-        res.status(400).json({ message: "User already exists." })
-        return
-      }
-
-      const salt = bcrypt.genSaltSync(saltRounds)
-      const hashedPassword = bcrypt.hashSync(password, salt)
-
-      return User.create({ email, password: hashedPassword, username })
-    })
-    .then((createdUser) => {
+    .create({ email, password, username })
+    .then(createdUser => {
       const { email, username, _id } = createdUser
       const user = { email, username, _id }
 
       res.status(201).json({ user })
     })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ message: "Internal Server Error" })
-    })
+    .catch(err => next(err))
 })
-
-
 
 
 
 
 router.post('/login', (req, res, next) => {
 
-  const { email, password } = req.body;
+  const { email, password } = req.body
 
   if (email === '' || password === '') {
-    res.status(400).json({ message: "Provide email and password." });
-    return;
+    res.status(400).json({ errorMessages: ['Indica email y contraseña'] })
+    return
   }
 
   User
     .findOne({ email })
-    .then((foundUser) => {
-
-      if (!foundUser) {
-        res.status(401).json({ message: "User not found." })
-        return;
-      }
-
-      if (bcrypt.compareSync(password, foundUser.password)) {
-
-        const { _id, email, username } = foundUser;
-
-        const payload = { _id, email, username }
-
-        const authToken = jwt.sign(
-          payload,
-          process.env.TOKEN_SECRET,
-          { algorithm: 'HS256', expiresIn: "6h" }
-        )
-
-        res.status(200).json({ authToken });
+    .then(foundUser => {
+      if (!foundUser || foundUser.validatePassword(password)) {
+        res.status(200).json({ authToken: foundUser.signToken() })
       }
       else {
-        res.status(401).json({ message: "Unable to authenticate the user" });
+        res.status(401).json({ errorMessages: ['Usuario o contraseña incorrectos'] })
       }
-
     })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ message: "Internal Server Error" })
-    });
-});
+    .catch(err => next(err))
+})
 
 
 
